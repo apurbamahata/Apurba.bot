c0a2412ffa458cc3bba15bad623e007eb8e55edc
apurbamahata <apurbamahata6@gmail.com>
2025-09-06 00:28:17 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Simple health check for Render
+echo "<h2>✅ Telegram Bot is running on Render!</h2>";
+echo "<p>If you see this page, your container is working.</p>";
+echo "<p>Bot script: <a href='bot.php'>Click here to run bot.php</a></p>";
